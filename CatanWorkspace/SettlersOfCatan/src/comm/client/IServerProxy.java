dc5e54aa2c4785d4c9c2b4a7c95ee17b1a86da40
Cory Beutler <cgbeutler@gmail.com>
2015-01-22 17:36:37 -0700

    Added all the TA files and a few others

---
+++ CatanWorkspace/SettlersOfCatan/src/comm/client/IServerProxy.java
@@ -0,0 +1,11 @@
+package comm.client;
+
+/**
+ * Interface containing all the calls that the server
+ * can accept through its http protocols
+ * @author Cory Beutler
+ *
+ */
+public interface IServerProxy {
+
+}
