eb7beb2d4ff2b464c55048dc47d76de37a67d3c6
THEMVFFINMAN <joshuajordanlowe@gmail.com>
2015-01-23 12:05:59 -0700

    JJ's Commital
    What up guys. This is my first co[m]mit. Be gentle.

---
+++ CatanWorkspace/SettlersOfCatan/src/shared/MessageList.java
@@ -0,0 +1,47 @@
+package shared;
+/**
+ * The chat window class that maintains all the messages
+ *
+ * @author JJ
+ */
+
+import java.util.ArrayList;
+
+public class MessageList {
+	
+	/** The list of messages. */
+	ArrayList<MessageLine> listOfMessages = new ArrayList<MessageLine>();
+	
+	public MessageList(){}
+	
+	/**
+	 * Adds a message.
+	 *
+	 * @param newMessage the new message
+	 */
+	public void addMessage(MessageLine newMessage)
+	{
+		
+	}
+	
+	/**
+	 * Gets the messages.
+	 *
+	 * @return the messages
+	 */
+	public ArrayList<MessageLine> getMessages()
+	{
+		return listOfMessages;
+	}
+	
+	/**
+	 * Sets the messages.
+	 *
+	 * @param listOfMessages the new messages
+	 */
+	public void setMessages(ArrayList<MessageLine> listOfMessages)
+	{
+		this.listOfMessages = listOfMessages;
+	}
+
+}
