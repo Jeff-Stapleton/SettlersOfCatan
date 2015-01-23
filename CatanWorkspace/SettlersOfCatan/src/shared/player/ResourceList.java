ec582c4c28622e32a702b8fcdb4708f5aa116be4
Jordan William Taylor <jordan25@sauce.cs.byu.edu>
2015-01-23 12:18:42 -0700

    Initial commit of shared/player

---
+++ CatanWorkspace/SettlersOfCatan/src/shared/player/ResourceList.java
@@ -0,0 +1,93 @@
+package shared.player;
+
+public class ResourceList {
+
+	private int brick;
+	private int ore;
+	private int wool;
+	private int grain;
+	private int wood;
+	
+	/**
+	 * Move resources (trade) to given player
+	 * @param player
+	 * @param trade
+	 */
+	public void moveResources(int player, ResourceList trade){
+				
+	}
+	
+	/**
+	 * Get number of brick
+	 * @return
+	 */
+	public int getBrick() {
+		return brick;
+	}
+	/**
+	 * Set number of brick
+	 * @param brick
+	 */
+	public void setBrick(int brick) {
+		this.brick = brick;
+	}
+	/**
+	 * Get number of ore
+	 * @return
+	 */
+	public int getOre() {
+		return ore;
+	}
+	/**
+	 * Set number of ore
+	 * @param ore
+	 */
+	public void setOre(int ore) {
+		this.ore = ore;
+	}
+	/**
+	 * Get number of wool
+	 * @return
+	 */
+	public int getWool() {
+		return wool;
+	}
+	/**
+	 * Set number of wool
+	 * @param wool
+	 */
+	public void setWool(int wool) {
+		this.wool = wool;
+	}
+	/**
+	 * Get number of grain
+	 * @return
+	 */
+	public int getGrain() {
+		return grain;
+	}
+	/**
+	 * Set number of grain
+	 * @param grain
+	 */
+	public void setGrain(int grain) {
+		this.grain = grain;
+	}
+	/**
+	 * Get number of wood
+	 * @return
+	 */
+	public int getWood() {
+		return wood;
+	}
+	/**
+	 * Set number of wood
+	 * @param wood
+	 */
+	public void setWood(int wood) {
+		this.wood = wood;
+	}
+	
+	
+	
+}
