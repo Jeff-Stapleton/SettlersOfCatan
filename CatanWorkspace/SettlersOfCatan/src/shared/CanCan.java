85c01665ad965844ff8f46f2187dec47a9f5092b
Jordan William Taylor <jordan25@curling.cs.byu.edu>
2015-02-02 13:23:42 -0700

    Updated CanCan

--- CatanWorkspace/SettlersOfCatan/src/shared/CanCan.java
+++ CatanWorkspace/SettlersOfCatan/src/shared/CanCan.java
@@ -1,10 +1,12 @@
 package shared;
 
+import shared.definitions.HexType;
+
 public class CanCan {
 
 //  Things we are suppose to implement as given by the TA's in the message from learning suite.
-//	CanDiscardCards, CanRollNumber, CanBuildRoad, CanBuildSettlement, CanBuildCity, CanOfferTrade, CanMaritimeTrade, 
-//	CanFinishTurn,CanBuyDevCard, CanUseYearOfPlenty, CanUseRoadBuilder, CanUseSoldier, CanUseMonopoly, CanUseMonument, CanPlaceRobber
+//	CanDiscardCards, CanRollNumber
+//	CanFinishTurn
 	
 	/**
 	 * Checks if a player can trade.
@@ -12,7 +14,7 @@ public class CanCan {
 	 * @param player the player
 	 * @return true, if successful
 	 */
-	public boolean canTrade(Player player){		
+	public static boolean canOfferTrade(Player player){		
 		return false;
 	}
 	
@@ -22,7 +24,7 @@ public class CanCan {
 	 * @param port the port
 	 * @return true, if successful
 	 */
-	public boolean canPortTrade(Player player){
+	public static boolean canMaritimeTrade(Player player){
 		return false;
 	}
 	
@@ -30,7 +32,7 @@ public class CanCan {
 	 * Checks to see if a dev card can be bought by this player
 	 * @return
 	 */
-	public boolean canBuyDevCard(Player player){
+	public static boolean canBuyDevCard(Player player){
 		if (player.getResources().getWool() >= 1 && player.getResources().getGrain() >= 1 && player.getResources().getOre() >= 1){
 			return true;
 		}
@@ -39,22 +41,54 @@ public class CanCan {
 		}
 	}
 	
-	public boolean canPlayDevCard(Player player){
+	public static boolean canUseYearOfPlenty(Player player){
 		if (player.hasPlayedDevCard()){
 			return false;
 		}
 		else{
 			return true;
 		}
-	}	
+	}
 	
-	/**
-	 * Checks to see if a settlement can be bought by this player
-	 * @return
-	 */
-	public boolean canBuySettlement(Player player){
-		if (player.getSettlements() >= 1 && player.getResources().getBrick() >= 1 && player.getResources().getWood() >= 1 
-				&& player.getResources().getGrain() >= 1 && player.getResources().getWool() >= 1){
+	public static boolean canUseRoadBuilder(Player player){
+		if (player.hasPlayedDevCard()){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public static boolean canUseSoldier(Player player){
+		if (player.hasPlayedDevCard()){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public static boolean canUseMonopoly(Player player){
+		if (player.hasPlayedDevCard()){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public static boolean canUseMonument(Player player){
+		if (player.hasPlayedDevCard()){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	
+	public static boolean canBuildSettlement(Player player){
+		if (player.getSettlements() >= 1 && player.getResources().getBrick() >= 1 && player.getResources().getWood() >= 1 && player.getResources().getGrain() >= 1 && player.getResources().getWool() >= 1){
 			return true;
 		}
 		else{
@@ -62,11 +96,7 @@ public class CanCan {
 		}
 	}
 	
-	/**
-	 * Checks to see if a city can be bought by this player
-	 * @return
-	 */
-	public boolean canBuyCity(Player player){
+	public static boolean canBuildCity(Player player){
 		if (player.getCities() >= 1 && player.getResources().getGrain() >= 2 && player.getResources().getOre() >= 3){
 			return true;
 		}
@@ -75,11 +105,7 @@ public class CanCan {
 		}
 	}
 	
-	/**
-	 * Checks to see if a road can be bought by this player
-	 * @return
-	 */
-	public boolean canBuyRoad(Player player){
+	public static boolean canBuildRoad(Player player){
 		if (player.getRoads() >= 1 && player.getResources().getBrick() >= 1 && player.getResources().getWood() >= 1){
 			return true;
 		}
@@ -88,19 +114,19 @@ public class CanCan {
 		}
 	}
 	
-	public boolean canBuildSettlement(Player player){
-		return false;
-	}
-	
-	public boolean canBuildCity(Player player){
-		return false;
-	}
-	
-	public boolean canBuildRoad(Player player){
-		return false;
-	}
-	
-	public boolean canPlaceRobber(Hex location){
-		return false;
+	public static boolean canPlaceRobber(Hex location, Robber robber){
+		if (robber.getLocation() == location.getLocation()){
+			// Robber must be moved from its location
+			return false;
+		}
+		if (location.getType() == HexType.DESERT){
+			// Robber cannot be placed on the Desert
+			return false;
+		}
+		if (location.getType() == HexType.WATER){
+			// Robber cannot be placed on water
+			return false;
+		}
+		return true;		
 	}
 }
