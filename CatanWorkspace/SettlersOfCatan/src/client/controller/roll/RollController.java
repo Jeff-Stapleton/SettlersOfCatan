bbf168d91ae70c1d8a72e238a969b9c8ac79b254
THEMVFFINMAN <joshuajordanlowe@gmail.com>
2015-02-23 12:04:05 -0700

    Implemented Roll
    Pretty simple stuff

--- CatanWorkspace/SettlersOfCatan/src/client/controller/roll/RollController.java
+++ CatanWorkspace/SettlersOfCatan/src/client/controller/roll/RollController.java
@@ -1,5 +1,8 @@
 package client.controller.roll;
 
+import java.util.Random;
+
+import client.comm.IServerProxy;
 import client.view.base.*;
 import client.view.roll.IRollResultView;
 import client.view.roll.IRollView;
@@ -11,18 +14,21 @@ import client.view.roll.IRollView;
 public class RollController extends Controller implements IRollController {
 
 	private IRollResultView resultView;
-
+	private IRollView rollView;
+	private IServerProxy serverProxy;
+	
+	private int rollValue;
 	/**
 	 * RollController constructor
 	 * 
 	 * @param view Roll view
 	 * @param resultView Roll result view
 	 */
-	public RollController(IRollView view, IRollResultView resultView) {
-
+	public RollController(IRollView view, IRollResultView resultView, IServerProxy serverProxy) 
+	{
 		super(view);
-		
 		setResultView(resultView);
+		this.serverProxy = serverProxy;
 	}
 	
 	public IRollResultView getResultView() {
@@ -38,9 +44,18 @@ public class RollController extends Controller implements IRollController {
 	
 	@Override
 	public void rollDice() {
-
-		getResultView().showModal();
+		Random randomGenerator1 = new Random();
+		Random randomGenerator2 = new Random();
+		int dice1 = randomGenerator1.nextInt(6) + 1;
+		int dice2 = randomGenerator2.nextInt(6) + 1;
+		int diceRoll = dice1 + dice2;
+//		System.out.println("dice1 " + dice1 + " dice2 " + dice2);
+		getResultView().setRollValue(diceRoll);
+//		getResultView().setRollValue(7);
+		rollView.closeModal();
+		getResultView().showModal();	
+		//presenter.rollNumber(diceRoll);
+		rollValue = diceRoll;
 	}
-
 }
 
