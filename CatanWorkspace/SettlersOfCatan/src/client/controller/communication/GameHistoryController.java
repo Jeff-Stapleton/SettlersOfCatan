328cfbaec1f2bb4d17fef722b99ced03af14e28d
THEMVFFINMAN <joshuajordanlowe@gmail.com>
2015-02-27 13:11:29 -0700

    GameHIstoryController DONE MOTHA
    YOU KNOW IT

--- CatanWorkspace/SettlersOfCatan/src/client/controller/communication/GameHistoryController.java
+++ CatanWorkspace/SettlersOfCatan/src/client/controller/communication/GameHistoryController.java
@@ -7,6 +7,7 @@ import client.view.base.*;
 import client.view.communication.IGameHistoryView;
 import client.view.communication.LogEntry;
 import shared.CatanModel;
+import shared.MessageLine;
 import shared.Player;
 import shared.definitions.*;
 
@@ -18,12 +19,12 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 
 	private CatanModel catanModel;
 	private CatanGame catanGame;
-	public GameHistoryController(CatanGame catanGame, IGameHistoryView view) {
+	public GameHistoryController(CatanGame catanGame, IGameHistoryView view) 
+	{
 		
 		super(view);
 		this.catanGame = catanGame;
 		catanGame.addObserver(this);
-		initFromModel();
 	}
 	
 	@Override
@@ -31,36 +32,25 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 		
 		return (IGameHistoryView)super.getView();
 	}
-	
-	private void initFromModel() {
-		
-		List<LogEntry> entries = new ArrayList<LogEntry>();
-		int playerIndex = -1;
-        
-        if (catanModel != null) {
-	        for (int i = 0; i < catanModel.getLog().getLines().size(); i++) 
-	        {
-	        	String name = catanModel.getLog().getLines().get(i).getSource();
-	        	for (int j = 0; j < 4; j++)
-	        	{
-	        		if (catanModel.getPlayers()[j].getName().equals(name))
-	        		{
-	        			playerIndex = catanModel.getPlayers()[j].getPlayerIndex();
-	        		}
-	        	}
-	        	
-	        	entries.add(new LogEntry(catanModel.getPlayers()[playerIndex].getColor(), catanModel.getLog().getLines().get(i).getMessage()));
-	        }
-    	}
-        
-        getView().setEntries(entries);
-	}
 
 	@Override
 	public void update(Observable obs, Object obj) {
-		if (obs instanceof CatanGame) {
+		if (obs instanceof CatanGame) 
+		{
 			catanModel = ((CatanGame) obs).getModel();
-			initFromModel();
+			
+	        List<LogEntry> entries = new ArrayList<LogEntry>();
+
+	        for (MessageLine line : catanModel.getLog().getLines())
+	        {
+	        	String user = line.getSource();
+	        	CatanColor color = null;
+	        	color = catanGame.getGameInfo().getPlayerWithName(user).getColor();
+	        	entries.add(new LogEntry(color, line.getMessage()));
+	        }
+	        
+	        getView().setEntries(entries);
+			
 		}
 	}
 	
