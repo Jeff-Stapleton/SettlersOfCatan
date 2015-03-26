99373fbd4a5904ab17da72809e5a480e23e174eb
Jordan William Taylor <jordan25@fishing.cs.byu.edu>
2015-03-26 01:42:52 -0600

    Added stuff for monopoly dev card

---
+++ CatanWorkspace/SettlersOfCatan/src/server/handlers/MovesMonopolyHandler.java
@@ -0,0 +1,72 @@
+package server.handlers;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import server.Server;
+import server.ServerGame;
+import server.comm.response.AbstractResponse;
+import server.comm.response.JsonResponse;
+import server.comm.response.MessageResponse;
+import shared.comm.ServerException;
+import shared.comm.serialization.MonumentRequest;
+
+public class MovesMonopolyHandler extends SimpleHandler
+{
+	private static final Logger log = Logger.getLogger(MovesMonumentHandler.class);
+	
+	private Server server = null;
+
+	public MovesMonopolyHandler(Server server)
+	{
+		this.server = server;
+	}
+
+	@Override
+	public void handle(HttpExchange exchange) throws IOException
+	{
+		try{
+			AbstractResponse response = null;
+			
+			log.debug("/moves/monopoly begun");
+			
+			log.trace("Verifying user credentials");
+			try
+			{
+				verifyUser(exchange, server);
+				log.trace("User is valid");
+	
+				log.trace("creating request body object");
+				MonumentRequest request = getRequest(exchange, MonumentRequest.class);
+				
+				ServerGame game = getGame(exchange, server);
+				
+				game.getMovesFacade().monopoly(request);
+				
+				log.trace("Chat message added to model");
+				response = new JsonResponse(200);
+				((JsonResponse)response).setJsonBody(game.getModel());
+			}
+			catch (ServerException e)
+			{
+				response = new MessageResponse(400, e.getMessage());
+			}
+	
+			log.trace("Adding response headers and cookies");
+			addResponseHeaders(exchange, response);
+			
+			log.trace("Sending response");
+			sendResponse(exchange, response);
+			
+			log.trace("/moves/sendChat finished");
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			throw(e);
+		}
+	}
+
+}
